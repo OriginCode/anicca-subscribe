b1bc250f904eafa2ffd33c1abea60e360f64251e
Kaiyang Wu <self@origincode.me>
2024-05-25 04:25:12 -0700

    chore: format code
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- main.rkt
+++ main.rkt
@@ -4,15 +4,12 @@
 (require text-table)
 
 (define subscribe-file
-  (open-input-file
-   (vector-ref (current-command-line-arguments) 0)))
+  (open-input-file (vector-ref (current-command-line-arguments) 0)))
 
 (define subscribe-packages
   (letrec ([load (λ (acc)
                    (let ([line (read-line subscribe-file)])
-                     (if (eof-object? line)
-                       acc
-                       (cons line (load acc)))))])
+                     (if (eof-object? line) acc (cons line (load acc)))))])
     (load '())))
 
 (define anicca-list
@@ -22,36 +19,32 @@
 
 (define (hash-assoc v key hs)
   (if (null? hs)
-    #f
-    (if (equal? v (hash-ref (car hs) key))
-      (car hs)
-      (hash-assoc v key (cdr hs)))))
+      #f
+      (if (equal? v (hash-ref (car hs) key))
+          (car hs)
+          (hash-assoc v key (cdr hs)))))
 
 (define (entry->list e)
   (list (hash-ref e 'name)
         (hash-ref e 'path)
         (hash-ref e 'before)
         (hash-ref e 'after)
-        (let [(warnings (hash-ref e 'warnings))]
-          (if (null? warnings)
-            "N/A"
-            warnings))))
+        (let ([warnings (hash-ref e 'warnings)])
+          (if (null? warnings) "N/A" warnings))))
 
 (define (fmttable res)
-  (print-table
-   #:row-sep? '(#t #f ...)
-   #:col-sep? '(#t #f ...)
-   #:align '(left left center center left)
-   (cons '(Name Path Before After Warnings) res)))
+  (print-table #:row-sep? '(#t #f ...)
+               #:col-sep? '(#t #f ...)
+               #:align '(left left center center left)
+               (cons '(Name Path Before After Warnings) res)))
 
 (define result
-  (letrec ([search (λ (acc ps)
-                     (if (null? ps)
-                       acc
-                       (let ([assoc-res (hash-assoc (car ps)
-                                          'name
-                                          anicca-list)])
-                         (if assoc-res
-                           (search (cons (entry->list assoc-res) acc) (cdr ps))
-                           (search acc (cdr ps))))))])
+  (letrec ([search
+            (λ (acc ps)
+              (if (null? ps)
+                  acc
+                  (let ([assoc-res (hash-assoc (car ps) 'name anicca-list)])
+                    (if assoc-res
+                        (search (cons (entry->list assoc-res) acc) (cdr ps))
+                        (search acc (cdr ps))))))])
     (fmttable (search '() subscribe-packages))))
