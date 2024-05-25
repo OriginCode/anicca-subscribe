9522041769d4abdc7f69e8e383334597d64dd654
Kaiyang Wu <self@origincode.me>
2024-05-25 13:39:33 -0700

    refactor: add some error handling
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- main.rkt
+++ main.rkt
@@ -3,19 +3,30 @@
 (require net/http-easy)
 (require text-table)
 
-(define subscribe-file
-  (open-input-file (vector-ref (current-command-line-arguments) 0)))
+(define (exn->user-error e)
+  (raise-user-error (exn-message e)))
+
+(define subscription-file
+  (let ([args (current-command-line-arguments)])
+    (if (> (vector-length args) 0)
+        (with-handlers ([exn:fail:filesystem:errno? exn->user-error]
+                        [exn:fail:filesystem? exn->user-error])
+          (open-input-file (vector-ref args 0) #:mode 'binary))
+        (raise-user-error 'anicca-subscribe "no subscription file provided"))))
 
 (define subscribe-packages
   (letrec ([load (λ (acc)
-                   (let ([line (read-line subscribe-file)])
+                   (let ([line (read-line subscription-file)])
                      (if (eof-object? line) acc (cons line (load acc)))))])
     (load '())))
 
 (define anicca-list
   (response-json
-   (get
-    "https://raw.githubusercontent.com/AOSC-Dev/anicca/main/pkgsupdate.json")))
+   (let ([res (get
+               "https://raw.githubusercontent.com/AOSC-Dev/anicca/main/pkgsupdate.json")])
+     (if (= (response-status-code res) 200)
+         res
+         (raise-user-error 'anicca-subscribe "failed to fetch anicca package update list")))))
 
 (define (hash-assoc v key hs)
   (if (null? hs)
