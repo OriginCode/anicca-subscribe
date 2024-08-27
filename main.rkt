e94b1804a56b8e6752711c8cb8218fb20116c4ef
Kaiyang Wu <self@origincode.me>
2024-08-26 17:14:47 -0700

    refactor: add some contracts
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- main.rkt
+++ main.rkt
@@ -7,13 +7,15 @@
 
 (require "convert.rkt")
 
-(define (subscription-file path)
+(define/contract (subscription-file path)
+  (-> path-string? input-port?)
   (let ([exn->user-error (λ (e) (raise-user-error (exn-message e)))])
     (with-handlers ([exn:fail:filesystem:errno? exn->user-error]
                     [exn:fail:filesystem? exn->user-error])
       (open-input-file path #:mode 'text))))
 
-(define (online-data)
+(define/contract (online-data)
+  (-> jsexpr?)
   (response-json
    (let ([res
           (get
@@ -23,20 +25,26 @@
          (raise-user-error 'anicca-subscribe
                            "failed to fetch anicca package update list")))))
 
-(define (format-table res)
+(define/contract (format-table res)
+  (-> (listof (listof string?)) void?)
   (print-table #:row-sep? '(#t #f ...)
                #:col-sep? '(#t #f ...)
                (cons '(Name Category Before After Warnings) res)))
 
-(define (search ps anicca-data)
+(define/contract (search ps anicca-data)
+  (-> (listof string?) (listof (listof string?)) (listof (listof string?)))
   (foldl (lambda (p acc)
            (let ([entry (assoc p anicca-data)])
              (if entry (cons entry acc) acc)))
          '()
          ps))
 
-(define package-names (make-parameter null))
-(define local-data (make-parameter #f))
+(define/contract package-names
+  (parameter/c (listof string?))
+  (make-parameter null))
+(define/contract local-data
+  (parameter/c jsexpr?)
+  (make-parameter #f))
 
 (define (cli)
   (command-line #:program "Anicca Subscribe"
@@ -57,7 +65,9 @@
 
 (cli)
 
-(define anicca-data (convert (if (local-data) (local-data) (online-data))))
+(define/contract anicca-data
+  (listof (listof string?))
+  (convert (if (local-data) (local-data) (online-data))))
 
 (let ([search-result (search (package-names) anicca-data)])
   (if (null? search-result)
