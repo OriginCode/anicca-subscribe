634800e7e9b66419e87e47b695fc2781b21ec5e4
Kaiyang Wu <self@origincode.me>
2024-10-11 18:07:38 -0700

    refactor(convert): use contracts instead to reduce contract boundary cost
    Signed-off-by: Kaiyang Wu <self@origincode.me>

--- convert.rkt
+++ convert.rkt
@@ -1,32 +1,32 @@
-#lang typed/racket/base
+#lang racket/base
 
-(require racket/match
+(require racket/contract
+         racket/match
          racket/string)
 
-(: convert
-   :
-   (Listof (HashTable Symbol (U String (Listof String))))
-   ->
-   (Listof (Listof String)))
-(define (convert pkgsupdate)
+(define/contract (convert pkgsupdate)
+  (-> (listof (hash/c symbol? (or/c string? (listof string?))))
+      (listof (listof string?)))
   (map
-   (λ ([pkg : (HashTable Symbol (U String (Listof String)))])
+   (λ (pkg)
      (match pkg
        [(hash 'name name
               'path path
               'before before
               'after after
               'warnings warnings)
-        (cast (list name
-                    (if (string? path) (car (string-split path "/")) "N/A")
-                    before
-                    after
-                    (if (list? warnings)
-                        (if (null? warnings)
-                            "N/A"
-                            (if (> (length warnings) 1) (cadr warnings) "N/A"))
-                        "N/A"))
-              (Listof String))]))
+        (list name
+              (if (string? path) (car (string-split path "/")) "N/A")
+              before
+              after
+              (if (list? warnings)
+                  (if (null? warnings)
+                      "N/A"
+                      (if (> (length warnings) 1) (cadr warnings) "N/A"))
+                  "N/A"))]))
    pkgsupdate))
 
-(provide convert)
+(provide (contract-out [convert
+                        (-> (listof (hash/c symbol? (or/c string? (listof string?))))
+                            (listof (listof string?)))
+                        ]))
